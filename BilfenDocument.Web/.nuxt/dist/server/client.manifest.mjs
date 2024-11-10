17d8d2d1630b5afb1a1f98142f3101332c0a8584
Erdemim1998 <erdemanacoglu90@gmail.com>
2024-11-10 16:40:22 +0300

    Gitignore silindi

---
+++ BilfenDocument.Web/.nuxt/dist/server/client.manifest.mjs
@@ -0,0 +1,1 @@
+export { default } from "file:///C:/Users/efsane/Desktop/BilfenProject/BilfenDocument.Web/node_modules/@nuxt/vite-builder/dist/runtime/client.manifest.mjs"
---
+++ BilfenDocument.Web/.nuxt/dist/server/server.mjs
@@ -0,0 +1,1 @@
+export { default } from "file:///C:/Users/efsane/Desktop/BilfenProject/BilfenDocument.Web/node_modules/@nuxt/vite-builder/dist/runtime/vite-node.mjs"
